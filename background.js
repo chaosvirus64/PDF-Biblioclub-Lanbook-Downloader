79f6a09b66e44bda6c2b8786fde44abb7be1fcac
chaosvirus64 <Alex_lolo@mail.ru>
2025-04-19 22:18:17 +1100

    Delete background.js

--- background.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.action==='keepAwake') chrome.power.requestKeepAwake('display');
-  if (msg.action==='releaseAwake') chrome.power.releaseKeepAwake();
-});
